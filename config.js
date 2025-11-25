9be445e24f9a048008cd373dfbe6221846068bda
ledian javier gonzalez rodriguez <ljgon.exstudents2025@student.ie.edu>
2025-11-25 22:28:42 +0100

    added blob storage

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 window.APP_CONFIG = {
   STORAGE_ACCOUNT: "group5five",
   TABLE_MEALS: "meals1",
+  BLOB_CONTAINER: "mealimages",
   TABLE_RESTAURANTS: "restaurants",
   SAS_TOKEN: "sv=2024-11-04&ss=bt&srt=so&sp=rlautfx&se=2025-12-30T22:59:17Z&st=2025-11-22T20:10:17Z&spr=https&sig=d1pNxyU4G%2BKeIt%2FSq4oM0looeo0fjczCasZKYltZ2iU%3D",
   BRAND: {
@@ -9,3 +10,4 @@ window.APP_CONFIG = {
     accent: "#ffb703"
   }
 };
+
