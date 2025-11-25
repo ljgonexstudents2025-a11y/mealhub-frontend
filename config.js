4ab627d1487d1d5c7212e3e02d7fec5e938e5294
ledian javier gonzalez rodriguez <ljgon.exstudents2025@student.ie.edu>
2025-11-25 22:29:17 +0100

    Reorder APP_CONFIG properties for consistency

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.APP_CONFIG = {
   STORAGE_ACCOUNT: "group5five",
   TABLE_MEALS: "meals1",
-  BLOB_CONTAINER: "mealimages",
   TABLE_RESTAURANTS: "restaurants",
+  BLOB_CONTAINER: "mealimages",
   SAS_TOKEN: "sv=2024-11-04&ss=bt&srt=so&sp=rlautfx&se=2025-12-30T22:59:17Z&st=2025-11-22T20:10:17Z&spr=https&sig=d1pNxyU4G%2BKeIt%2FSq4oM0looeo0fjczCasZKYltZ2iU%3D",
   BRAND: {
     primary: "#0b6cff",
@@ -10,4 +10,5 @@ window.APP_CONFIG = {
     accent: "#ffb703"
   }
 };
+
 
